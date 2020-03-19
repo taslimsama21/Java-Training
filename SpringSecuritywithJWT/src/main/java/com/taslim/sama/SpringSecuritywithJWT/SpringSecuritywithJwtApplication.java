6bbb93a83596803c1cb5af578b7e20a1a595f8ab
taslimsama21 <taslimsama21@gmail.com>
2020-03-19 12:08:02 +0530

    srfhbch

---
+++ SpringSecuritywithJWT/src/main/java/com/taslim/sama/SpringSecuritywithJWT/SpringSecuritywithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.taslim.sama.SpringSecuritywithJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritywithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritywithJwtApplication.class, args);
+	}
+
+}
---
+++ SpringSecuritywithJWT/src/test/java/com/taslim/sama/SpringSecuritywithJWT/SpringSecuritywithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taslim.sama.SpringSecuritywithJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritywithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
